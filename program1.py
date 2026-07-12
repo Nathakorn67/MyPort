a7419d9f7fc9d49bd39bf5c58c2fbc0699128aed
Nathakorn67 <467028@wsk.ac.th>
2026-07-12 20:19:08 +0700

    program04.py

--- program1.py
+++ program1.py
@@ -1,9 +1,8 @@
 num1 = int(input("first number: ")) #เลขตัวที่1
 num2 = int(input("Second number: ")) #เลขตัวที่2
-Ans = (num1**num2)**0.5 #ยกกำลัง+ถอดรากที่2
-if Ans > 0:
-    print("เต็มบวก :", Ans)
-elif Ans < 0:
-    print("เต็มลบ :", Ans)
-else:
-    print("เต็มลบ :", Ans)
+
+area = num1 * num2
+per = 2 * (num1 * num2)
+
+print("พื้นที่คือ ", area)
+print("เส้นรอบรูปคือ ", per)
